fc5a3f920bbdcf161ee32efcbeb32dd05a57844f
Arman Rahman <armanrahman0@gmail.com>
2023-12-18 00:42:49 +0600

    update

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../Components/Header";
+import Master from "../Layouts/Master";
+
+function Home() {
+  return (
+    <div>
+      <Master>
+        <Header />
+      </Master>
+    </div>
+  );
+}
+
+export default Home;
